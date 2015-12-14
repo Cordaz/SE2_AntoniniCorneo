6bae503d0ccf1464dc2d2ab6922bee31fbb182e0
Paolo Antonini <paolo1.antonini@mail.polimi.it>
2015-12-14 23:21:18 +0100

    Getting LaTeX and Minted to do what I want.

--- 03_CI/java/SecurityMechanismSelector.java
+++ 03_CI/java/SecurityMechanismSelector.java
@@ -1201,18 +1201,18 @@ localStrings.getLocalString("securitymechansimselector.runas_cannot_propagate_us
     }
 
    
-   /* Evaluates a client's conformance to a security policies
+#\label{l:startASC}%   /* Evaluates a client's conformance to a security policies
     * at the client authentication layer.
     *
     * returns true if conformant ; else returns false
     */
-    private boolean evaluate_client_conformance_ascontext(
+#\label{l:108}%    private boolean evaluate_client_conformance_ascontext(
                         SecurityContext ctx,
                         EjbIORConfigurationDescriptor iordesc,
                         String realmName)
-    {
+    {#\label{l:112}%
 
-        boolean client_authenticated = false;
+        boolean client_authenticated = false;#\label{l:114}%
 
         // get requirements and supports at the client authentication layer
         AS_ContextSec ascontext = null;
@@ -1242,10 +1242,10 @@ localStrings.getLocalString("securitymechansimselector.runas_cannot_propagate_us
          * 
          *************************************************************************/
 
-        if  ( (ctx != null) && (ctx.authcls != null) &&  (ctx.subject != null))
+#\label{l:144}%        if  ( (ctx != null) && (ctx.authcls != null) &&  (ctx.subject != null))
             client_authenticated = true;
         else
-            client_authenticated = false;
+            client_authenticated = false;#\label{l:147}%
 
         if (client_authenticated) {
             if ( ! ( isSet(ascontext.target_requires, EstablishTrustInClient.value)
@@ -1254,35 +1254,35 @@ localStrings.getLocalString("securitymechansimselector.runas_cannot_propagate_us
             }
             // match the target_name from client with the target_name in policy 
 
-            byte [] client_tgtname = getTargetName(ctx.subject);
+            byte [] client_tgtname = getTargetName(ctx.subject);#\label{l:156}%
 
             if (ascontext.target_name.length != client_tgtname.length){
                 return false; // mechanism did not match.
             }            
-            for (int i=0; i < ascontext.target_name.length ; i ++)
+#\label{l:161}%            for (int i=0; i < ascontext.target_name.length ; i ++)
                 if (ascontext.target_name[i] != client_tgtname[i]){
                     return false; // mechanism did not match
-                }
+                }#\label{l:164}%
         } else { 
             if ( isSet(ascontext.target_requires, EstablishTrustInClient.value)){
                 return false;  // no mechanism match.
             }
         }
         return true;
-    }
+    } #\label{l:endASC}%
 
-   /* Evaluates a client's conformance to a security policy
+#\label{l:startSAS}%   /* Evaluates a client's conformance to a security policy
     * at the sas context layer. The security policy
     * is derived from the EjbIORConfigurationDescriptor.
     *
     * returns true if conformant ; else returns false
     */
-    private boolean evaluate_client_conformance_sascontext(
+#\label{l:209}%    private boolean evaluate_client_conformance_sascontext(
                         SecurityContext ctx,
                         EjbIORConfigurationDescriptor iordesc)
-    {
+    {#\label{l:212}%
 
-        boolean caller_propagated = false;
+        boolean caller_propagated = false;#\label{l:214}%
 
         // get requirements and supports at the sas context layer
         SAS_ContextSec sascontext = null;
@@ -1294,14 +1294,14 @@ localStrings.getLocalString("securitymechansimselector.runas_cannot_propagate_us
         }
 
             
-        if  ( (ctx != null) && (ctx.identcls != null) &&  (ctx.subject != null))
+#\label{l:226}%        if  ( (ctx != null) && (ctx.identcls != null) &&  (ctx.subject != null))
             caller_propagated = true;
         else
-            caller_propagated = false;
+            caller_propagated = false;#\label{l:229}%
 
         if (caller_propagated) {
-            if ( ! isSet(sascontext.target_supports, IdentityAssertion.value))
-                return false; // target does not support IdentityAssertion
+            if ( ! isSet(sascontext.target_supports, IdentityAssertion.value))#\label{l:232}%
+                return false;#\label{l:233}% // target does not support IdentityAssertion
 
             /* There is no need further checking here since SecServerRequestInterceptor
              * code filters out the following:
@@ -1315,11 +1315,11 @@ localStrings.getLocalString("securitymechansimselector.runas_cannot_propagate_us
             return true;
         }
         return true; //  either caller was not propagated or mechanism matched.
-    }
+    }#\label{l:endSAS}%
        
                                                     
 
-    /**
+#\label{l:startECC}%    /**
      * Evaluates a client's conformance to the security policies configured
      * on the target.
      * Returns true if conformant to the security policies
@@ -1335,28 +1335,28 @@ as_context_mech
      * and sas_context_mech must all be consistent.
      * 
      */
-    private boolean evaluate_client_conformance(SecurityContext   ctx,
-                                                byte[]            object_id,
-                                                boolean           ssl_used,
+    private boolean evaluate_client_conformance(SecurityContext   ctx,#\label{l:319}%
+                                                byte[]            object_id,#\label{l:320}%
+                                                boolean           ssl_used,#\label{l:321}%
                                                 X509Certificate[] certchain)
-    {
+    {#\label{l:323}%
         // Obtain the IOR configuration descriptors for the Ejb using
         // the object_id within the SecurityContext field.
 
         // if object_id is null then nothing to evaluate. This is a sanity
         // check - for the object_id should never be null.
         
-        if (object_id == null)
-            return true;
+        if (object_id == null)#\label{l:330}%
+            return true;#\label{l:331}%
 
-        if (protocolMgr == null)
-            protocolMgr = orbHelper.getProtocolManager();
+        if (protocolMgr == null)#\label{l:333}%
+            protocolMgr = orbHelper.getProtocolManager();#\label{l:334}%
 
         // Check to make sure protocolMgr is not null. 
         // This could happen during server initialization or if this call
         // is on a callback object in the client VM. 
-        if (protocolMgr == null)
-            return true;
+        if (protocolMgr == null)#\label{l:339}%
+            return true;#\label{l:340}%
 
         EjbDescriptor ejbDesc = protocolMgr.getEjbDescriptor(object_id);
 
@@ -1379,8 +1379,8 @@ as_context_mech
          * no security policy is configured. So consider the client 
          * to be conformant.
          */
-        if (iorDescSet.isEmpty())
-            return true;
+        if (iorDescSet.isEmpty())#\label{l:363}%
+            return true;#\label{l:364}%
 
         // go through each EjbIORConfigurationDescriptor trying to find
         // a find a CompoundSecMechanism that matches client's actions.
@@ -1432,10 +1432,10 @@ as_context_mech
             }
             return true;  // security policy matched.
         }
-        if(checkSkipped)
-            return true;
+        if(checkSkipped)#\label{l:3116}%
+            return true;#\label{l:3117}%
         return false; // No matching security policy found
-    }     
+    }     #\label{l:endECC}%
      
      /** if ejb requires no security - then skip checking the client-conformance
      */    
